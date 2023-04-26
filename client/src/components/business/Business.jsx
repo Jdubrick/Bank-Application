5904dc6b6b696d758ebbb318375c2aef99fa3c27
Jordan Dubrick <dubrickjordan@gmail.com>
2023-04-25 23:18:44 -0400

    prod fix v2

--- client/src/components/business/Business.jsx
+++ client/src/components/business/Business.jsx
@@ -33,7 +33,7 @@ function Business() {
       <div className="flex flex-col flex-1 justify-center items-start">
         <h2 className="font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full">
           You do the business, <br className="sm:block hidden" />
-          we'll handle the money.
+          we{"'"}ll handle the money.
         </h2>
         <p className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] max-w-[470px] mt-5">
           With the right credit card, you can improve your financial life by
